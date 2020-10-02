033a0f0f92c1faf307270b122079310f57c6a7a1
Hramchenlo Alexey <qwefix@gmail.com>
2020-10-02 17:03:03 +0300

    feat: e-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if(str===undefined) return
+  let aditionalArray=[];
+  let secondArr=[];
+  let additionStr='';
+  let strPlusAddition='';
+  let result='';
+  if (!options.additionSeparator)options.additionSeparator='|'
+  if (!options.separator)options.separator='+';
+  if (!options.additionRepeatTimes)options.additionRepeatTimes=1;
+
+  if (options.addition!==undefined){
+    
+    for(let i=0;i<options.additionRepeatTimes;i++){
+      aditionalArray.push( String(options.addition));
+    }
+    additionStr=aditionalArray.join(options.additionSeparator)
+  }
+  strPlusAddition=String(str)+additionStr;
+
+  if(options.repeatTimes&&options.repeatTimes%1==0){
+    for(let i=0;i<options.repeatTimes;i++){
+      secondArr.push(strPlusAddition);    
+    }
+    result=secondArr.join(options.separator)
+  }else{
+    result=strPlusAddition;
+  }
+  return result
 };
   
